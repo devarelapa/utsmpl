d79c581ca74492bd0bd18c279ccd2b07cfc0e788
devarelapa <167858361+devarelapa@users.noreply.github.com>
2024-04-23 16:25:26 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+ void main(List<String> args){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+ }
+ class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      backgroundColor: Colors.blue[200],
+      appBar:new AppBar(
+        backgroundColor: Colors.blue[300],
+        leading: new Icon (Icons.people_outline),
+        title: new Center(
+          child:  Text(
+            "BIODATA",
+            style: new TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      body: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+         new Image.asset(
+            "images/test.jpg",
+            width: 195.0,
+            height: 195.0,
+          ),
+          const Text(
+            "Nama  : Devarel Albany Putra Alamsyah",
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.black,),
+              textAlign: TextAlign.center,
+          ),
+              const Text(
+            "Nim  : STI202102472",
+            style:  TextStyle(
+              fontSize: 20.0,
+              color: Colors.black),
+              textAlign: TextAlign.center,
+              ),
+              const Text(
+            "Alamat  : Sudagaran, Sidareja, Cilacap",
+            style:  TextStyle(
+              fontSize: 20.0,
+              color: Colors.black),
+              textAlign: TextAlign.center,
+              ),
+              const Text(
+            "Jenis Kelamin  : Laki - Laki",
+            style:  TextStyle(
+              fontSize: 20.0,
+              color: Colors.black),
+              textAlign: TextAlign.center,
+          ),
+        ],
+      ),);}}
